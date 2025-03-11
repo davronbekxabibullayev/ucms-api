c6054bbaa6ab679ba9261795b3e100dbce75cbf0
davronbek xabibullayev <davronbekxabibullayev03.06.88@gmail.com>
2025-03-11 09:41:56 +0500

    refactored

--- src/Services/Stock/Ucms/Extensions/PermissionsConfiguration.cs
+++ src/Services/Stock/Ucms/Extensions/PermissionsConfiguration.cs
@@ -1,23 +1,12 @@
 namespace Ucms.Stock.Api.Extensions;
 
-using Devhub.Authorization.Extensions;
-
 public static class PermissionsConfiguration
 {
     public static IServiceCollection AddApplicationAuth(this IServiceCollection services)
     {
         services.AddAuthentication();
-        services.AddApplicationPermissions();
 
         return services;
     }
 
-    public static IServiceCollection AddApplicationPermissions(this IServiceCollection services)
-    {
-        services
-            .AddPermissions()
-            .AddHttpPermissionProvider();
-
-        return services;
-    }
 }
