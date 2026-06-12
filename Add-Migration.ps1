56892031c8eec30a0a81877cdf3bc1e97ef6faef
davronbek xabibullayev <davronbekxabibullayev03.06.88@gmail.com>
2026-06-12 14:51:12 +0500

    refactored

--- Add-Migration.ps1
+++
@@ -1,9 +0,0 @@
-param(
-    [Parameter(Mandatory = $true, Position = 0)]
-    [string]$Name
-)
-
-dotnet ef migrations add $Name `
-    --project src/Ucms.Infrastructure `
-    --startup-project src/Ucms.Api `
-    --output-dir Migrations
