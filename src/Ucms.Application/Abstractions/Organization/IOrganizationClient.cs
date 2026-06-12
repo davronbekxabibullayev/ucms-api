fdd462c2a5aa25a27d184340419b233a9aa3e7fa
Davronbek Xabibullayev Xabibulla o'g'li <davronbekxabibullayev03.06.88@gmail.com>
2026-06-12 05:33:28 +0500

    refactored

--- src/Ucms.Application/Abstractions/Organization/IOrganizationClient.cs
+++ src/Ucms.Application/Abstractions/Organization/IOrganizationClient.cs
@@ -2,7 +2,7 @@ namespace Ucms.Application.Abstractions.Organization;
 
 public interface IOrganizationClient
 {
-    Task<IEnumerable<Guid?>> GetOrganizationIds(Guid? organizationId = null, bool includeChilds = false, Guid? regionId = null, Guid? cityId = null);
-    Task<IEnumerable<Guid>> GetStockIds(Guid? organizationId = null);
-    Task<bool> CheckOrganizationBrigadeStock(Guid stockId);
+    public Task<IEnumerable<Guid?>> GetOrganizationIds(Guid? organizationId = null, bool includeChilds = false, Guid? regionId = null, Guid? cityId = null);
+    public Task<IEnumerable<Guid>> GetStockIds(Guid? organizationId = null);
+    public Task<bool> CheckOrganizationBrigadeStock(Guid stockId);
 }
